b35f100b232524b46dd8efd7ebd9166dbfabdd62
Dmitrii <kodim97@yandex.ru>
2021-05-25 19:49:04 +0300

    clean remote rep and useless features for build

--- grassdx11/GrassDX11/ModelLoader.cpp
+++ grassdx11/GrassDX11/ModelLoader.cpp
@@ -18,7 +18,6 @@ bool ModelLoader::Load(HWND hwnd, ID3D11Device * dev, ID3D11DeviceContext * devc
 		aiProcess_ConvertToLeftHanded);
 
 	if (pScene == NULL) {
-
 		auto wtf = importer.GetErrorString();
 		return false;
 	}
@@ -136,8 +135,10 @@ vector<Texture> ModelLoader::loadMaterialTextures(aiMaterial * mat, aiTextureTyp
 			}
 			else
 			{
-				string filename = string(str.C_Str());
-				//filename = directory + '/' + filename;
+				string filepath = string(str.C_Str());
+				std::size_t found = filepath.find_last_of("/\\");
+				string filename = filepath.substr(found + 1);
+				filename = directory + '/' + filename;
 				wstring filenamews = wstring(filename.begin(), filename.end());
 				hr = CreateWICTextureFromFile(dev, devcon, filenamews.c_str(), nullptr, &texture.texture);
             if (FAILED(hr))
