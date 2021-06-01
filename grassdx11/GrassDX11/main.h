d93fd59231b20457cde339fb6800d2af8597875d
Dmitrii <kodim97@yandex.ru>
2021-06-01 22:25:25 +0300

    setup release cfg

--- grassdx11/GrassDX11/main.h
+++ grassdx11/GrassDX11/main.h
@@ -34,6 +34,8 @@
 //--------------------------------------------------------------------------------------
 int g_windowWidth = 1920;
 int g_windowHeight = 1080;
+//int g_windowWidth = 1600;
+//int g_windowHeight = 900;
 
 
 //
@@ -59,7 +61,7 @@ int                                  g_fNumOfMeshes = 0;
 DebugWindow                         *g_dbgWin;
 
 // Snow Particle System
-int g_totalParticles = 250000;
+int g_totalParticles = 10000;
 ParticleShader* g_ParticleShader = nullptr;
 SnowParticleSystem* g_ParticleSystem = nullptr;
 
